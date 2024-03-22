8a32c62bbefdc507a52bff5cb24278486c7077b2
lomak <lomako221@mail.ru>
2024-03-22 23:09:10 +0300

    added program

---
+++ SalaryComparator.java
@@ -0,0 +1,10 @@
+import java.util.Comparator;
+
+public class SalaryComparator implements Comparator<Employee> {
+   public SalaryComparator() {
+   }
+
+   public int compare(Employee o1, Employee o2) {
+      return Double.compare(o2.calculateSalary(), o1.calculateSalary());
+   }
+}
